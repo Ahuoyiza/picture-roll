c8e3cc9c40a2a3e399e8866f57690fadde99e608
Ahuoyiza <sophieoyiza@gmail.com>
2020-09-16 02:07:51 +0100

    Updates

---
+++ precache-manifest.51a9af0e1da218dd6736789b0e8c3666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4d677bba99c805b2e4b01c247ac3a6",
+    "url": "/picture-roll/index.html"
+  },
+  {
+    "revision": "7e40c7edb7fb01a37028",
+    "url": "/picture-roll/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cc57afaa128b3ac3c6df",
+    "url": "/picture-roll/static/css/main.75e8d466.chunk.css"
+  },
+  {
+    "revision": "7e40c7edb7fb01a37028",
+    "url": "/picture-roll/static/js/2.3c6df923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picture-roll/static/js/2.3c6df923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc57afaa128b3ac3c6df",
+    "url": "/picture-roll/static/js/main.f69015f9.chunk.js"
+  },
+  {
+    "revision": "d511ee59d015b5c9f460",
+    "url": "/picture-roll/static/js/runtime-main.7c8c227f.js"
+  }
+]);
